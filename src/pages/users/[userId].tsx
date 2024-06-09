07a11fd1c5c7f53c663b58fdb4b4481ef2b14797
tenzyu <tenzyu.on@gmail.com>
2024-06-09 18:26:33 +0900

    feat: post feed

--- src/pages/users/[userId].tsx
+++ src/pages/users/[userId].tsx
@@ -5,16 +5,17 @@ import useUser from "@/hooks/useUser"
 import { ClipLoader } from "react-spinners"
 import UserHero from "@/components/users/UserHero"
 import UserBio from "@/components/users/UserBio"
+import PostFeed from "@/components/posts/PostFeed"
 
 const UserView = () => {
     const router = useRouter()
     const { userId } = router.query
-    const { data: fetchedUser, isLoading} = useUser(userId as string)
+    const { data: fetchedUser, isLoading } = useUser(userId as string)
 
     if (isLoading || !fetchedUser) {
         return (
             <div className="flex justify-center items-center h-full">
-                <ClipLoader color="lightblue" size={80}/>
+                <ClipLoader color="lightblue" size={80} />
             </div>
         )
     }
@@ -24,6 +25,7 @@ const UserView = () => {
             <Header showBackArrow label={fetchedUser.name} />
             <UserHero userId={userId as string} />
             <UserBio userId={userId as string} />
+            <PostFeed userId={userId as string} />
         </>
     )
 }
