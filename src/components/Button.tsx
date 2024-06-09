9f7bcd1f0988b023a0f75df078d9b96296ef2d0a
tenzyu <tenzyu.on@gmail.com>
2024-06-09 18:53:24 +0900

    fix: style of Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -29,7 +29,7 @@ const Button: React.FC<ButtonProps> = ({
             font-semibold
             hover:opacity-80
             transition
-            boirder-2
+            border-2
             ${fullWidth ? 'w-full' : 'w-fit'}
             ${secondary ? 'bg-white' : 'bg-sky-500'}
             ${secondary ? 'text-black' : 'text-white'}
