e743e02a152a2b47e15dbac80c58e35d9a37ea22
tenzyu <tenzyu.on@gmail.com>
2024-06-09 17:52:03 +0900

    fix: border bottom on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header: React.FC<HeaderProps> = ({
     }, [router])
 
     return (
-        <div className="border-b-{1px} border-neutral-800 p-5">
+        <div className="border-b-[1px] border-neutral-800 p-5">
             <div className="flex flex-row items-center gap-2">
                 {
                     showBackArrow && (
